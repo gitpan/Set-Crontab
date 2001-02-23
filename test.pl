db45ba3fc5c318d34c0fe685528717e55f892cc9
Abhijit Menon-Sen <ams@toroid.org>
2001-02-23 23:54:22 +0000

    Import of AMS/Set-Crontab-0.90 from CPAN.
    gitpan-cpan-distribution: Set-Crontab
    gitpan-cpan-version:      0.90
    gitpan-cpan-path:         AMS/Set-Crontab-0.90.tar.gz
    gitpan-cpan-author:       AMS
    gitpan-cpan-maturity:     released

---
+++ test.pl
@@ -0,0 +1,28 @@
+use Test;
+
+BEGIN { plan tests => 10 }
+
+use Set::Crontab;
+
+my $r = [0..10];
+
+ok(Set::Crontab->new("1,2,3,4,5,6", $r)->contains(6));
+ok(Set::Crontab->new("1-10", $r)->contains(5));
+ok(Set::Crontab->new("1-2,3-4,5-6,7-8,9-10", $r)->contains(7));
+ok(Set::Crontab->new("1-4,5-10", $r)->contains(7));
+ok(Set::Crontab->new("*/3", $r)->contains(6));
+
+my $s = Set::Crontab->new("!3", $r);
+ok(!$s->contains(3) && $s->contains(1));
+
+$s = Set::Crontab->new(">3,<8", $r);
+ok(!$s->contains(2) && $s->contains(6));
+
+$s = Set::Crontab->new(">3,<8,!6", $r);
+ok(!$s->contains(6) && $s->contains(7));
+
+$s = Set::Crontab->new("*,!8", $r);
+ok(!$s->contains(8) && $s->contains(3));
+
+$s = Set::Crontab->new("1,*/2,!4", $r);
+ok(!$s->contains(4) && $s->contains(2));
